76d8ddfdfd2b4e87ac280ac51792653b9b55d60b
pray3m <prem.gtm9@gmail.com>
2023-10-03 20:08:50 +0545

    Setting up providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ChakraProvider } from "@chakra-ui/react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
