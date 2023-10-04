d34f4fe874a049dbc80bdc54e76e0cc9db67446b
pray3m <prem.gtm9@gmail.com>
2023-10-04 12:43:17 +0545

    feat : create custom hook for currency conversion

--- features/common/hooks/useCurrency.js
+++ features/common/hooks/useCurrency.js
@@ -4,8 +4,8 @@ import { fetchRates, fetchSymbols } from "../../Converter/api/fetchData";
 
 export const useCurrency = () => {
   const [amount, setAmount] = useState(25);
-  const [currencyOne, setCurrencyOne] = useState("EUR");
-  const [currencyTwo, setCurrencyTwo] = useState("EUR");
+  const [currencyOne, setCurrencyOne] = useState("USD");
+  const [currencyTwo, setCurrencyTwo] = useState("NPR");
 
   const [ratesData, symbolsData] = useQueries({
     queries: [
@@ -27,5 +27,31 @@ export const useCurrency = () => {
   const isLoading = [ratesData, symbolsData].some((query) => query.isLoading);
   const isError = [ratesData, symbolsData].some((query) => query.isError);
 
-  return { amount, currencyOne, currencyTwo };
+  const convertedAmount = (ratesData.data?.rates[currencyTwo] * amount).toFixed(
+    2
+  );
+
+  const date = new Date(ratesData.data?.date).toLocaleDateString();
+  const time = new Date(ratesData.data?.timestamp).toLocaleTimeString("en-US");
+
+  const currencyList = symbolsData.data
+    ? Object.keys(symbolsData.data.symbols)
+    : {};
+
+  return {
+    isLoading,
+    isError,
+    amount,
+    setAmount,
+    currencyOne,
+    currencyTwo,
+    setCurrencyOne,
+    setCurrencyTwo,
+    currencyList,
+    convertedAmount,
+    ratesData,
+    symbolsData,
+    date,
+    time,
+  };
 };
