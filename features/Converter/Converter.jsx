aacfce77b15f233366e36973bb292d30115412da
pray3m <prem.gtm9@gmail.com>
2023-10-04 14:48:18 +0545

    feat : add error and loading UI pattern

--- features/Converter/Converter.jsx
+++ features/Converter/Converter.jsx
@@ -1,15 +1,45 @@
+import { Box, Spinner, Text } from "@chakra-ui/react";
 import { useCurrency } from "../common/hooks/useCurrency";
 
 const Converter = () => {
-  const { amount, currencyOne, currencyTwo } = useCurrency();
+  const {
+    isLoading,
+    isError,
+    amount,
+    setAmount,
+    currencyOne,
+    currencyTwo,
+    setCurrencyOne,
+    setCurrencyTwo,
+    currencyList,
+    convertedAmount,
+    ratesData,
+    symbolsData,
+    date,
+    time,
+  } = useCurrency();
 
-  return (
-    <>
-      <p>{amount}</p>
-      <p>{currencyOne}</p>
-      <p>{currencyTwo}</p>
-    </>
-  );
+  if (isError) {
+    return (
+      <Text fontWeight="bold" fontSize="3xl" color="white" my="10">
+        Something went wrong !
+      </Text>
+    );
+  }
+
+  if (isLoading)
+    return (
+      <Spinner
+        margin="auto 0"
+        size="xl"
+        thickness="4px"
+        speed="0.65s"
+        color="purple.500"
+        emptyColor="purple.200"
+      />
+    );
+
+  return <Box width={{ base: "90vw", sm: "45vw" }} margin="0 auto"></Box>;
 };
 
 export default Converter;
