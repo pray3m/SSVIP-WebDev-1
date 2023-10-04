4893308337f29e123f26df151780086728408b51
pray3m <prem.gtm9@gmail.com>
2023-10-04 22:57:41 +0545

    fix : dark theme in Chakra UI

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import theme from "../theme.js";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
         <ReactQueryDevtools initialIsOpen={false} />
       </ChakraProvider>
