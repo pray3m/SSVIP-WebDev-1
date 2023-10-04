38aa88571eb2df1d150ba35685c20ea98b3d21b0
pray3m <prem.gtm9@gmail.com>
2023-10-04 12:03:39 +0545

    fix axios error and setup react query

--- features/Converter/api/fetchData.js
+++ features/Converter/api/fetchData.js
@@ -1,14 +1,16 @@
-import { axios } from "../../../lib/axios";
+import { instance } from "../../../lib/axios";
 
 export const fetchRates = async (currencyOne) => {
-  const { data } = await axios.get(
-    `/latest?base=${currencyOne}&apikey${import.meta.env.VITE_API_KEY}`
+  const { data } = await instance.get(
+    `/latest?base=${currencyOne}&apikey=${import.meta.env.VITE_API_KEY}`
   );
+
+  return data;
 };
 
 export const fetchSymbols = async () => {
-  const { data } = await axios.get(
-    `/symbols?apikey${import.meta.env.VITE_API_KEY}`
+  const { data } = await instance.get(
+    `/symbols?apikey=${import.meta.env.VITE_API_KEY}`
   );
 
   return data;
--- features/common/hooks/useCurrency.js
+++ features/common/hooks/useCurrency.js
@@ -1,9 +1,31 @@
+import { useQueries } from "@tanstack/react-query";
 import { useState } from "react";
+import { fetchRates, fetchSymbols } from "../../Converter/api/fetchData";
 
 export const useCurrency = () => {
   const [amount, setAmount] = useState(25);
-  const [currencyOne, setCurrencyOne] = useState("USD");
+  const [currencyOne, setCurrencyOne] = useState("EUR");
   const [currencyTwo, setCurrencyTwo] = useState("EUR");
+
+  const [ratesData, symbolsData] = useQueries({
+    queries: [
+      {
+        queryKey: ["rates", currencyOne],
+        queryFn: () => fetchRates(currencyOne),
+        staleTime: Infinity,
+        select: ({ rates, date, timestamp }) => ({ rates, date, timestamp }),
+      },
+      {
+        queryKey: ["symbols"],
+        queryFn: () => fetchSymbols(),
+        staleTime: Infinity,
+        select: ({ symbols }) => ({ symbols }),
+      },
+    ],
+  });
+
+  const isLoading = [ratesData, symbolsData].some((query) => query.isLoading);
+  const isError = [ratesData, symbolsData].some((query) => query.isError);
 
   return { amount, currencyOne, currencyTwo };
 };
